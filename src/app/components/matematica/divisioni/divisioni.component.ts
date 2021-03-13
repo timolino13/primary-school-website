32bc4d11beb3e38f687b52b519c933d008a96575
Uygar Karalalek <uy.kara@gmail.com>
2021-03-13 18:18:07 +0100

    Created graphics for component.

--- src/app/components/matematica/divisioni/divisioni.component.ts
+++ src/app/components/matematica/divisioni/divisioni.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {NumberPair} from '../../../models/NumberPair';
+import {Answer} from '../../../models/Answer';
 
 @Component({
   selector: 'app-divisioni',
@@ -8,27 +9,43 @@ import {NumberPair} from '../../../models/NumberPair';
 })
 export class DivisioniComponent implements OnInit {
 
+  static UNDEFINED = 0;
+
   divisionPair: NumberPair = new NumberPair(0, 0);
-  result = 0;
+
+  answer: Answer<number> = new Answer<number>(
+    false,
+    false,
+    DivisioniComponent.UNDEFINED);
 
   constructor() {
   }
 
   ngOnInit(): void {
-    this.initPair();
+    this.next();
   }
 
-  initPair(): void {
+  next(): void {
+    this.answer.registered = false;
+    this.answer.correct = false;
+    this.answer.input = DivisioniComponent.UNDEFINED;
+
+    this.generateCalculation();
+  }
+
+  // tslint:disable-next-line:typedef
+  private generateCalculation() {
     const divisor = Math.ceil(Math.random() * 10) + 1;
-    const multiple = Math.ceil(Math.random() * 10) + 1;
+    const multiple = Math.ceil(Math.random() * 9) + 1;
     const dividend = multiple * divisor;
 
     this.divisionPair = new NumberPair(dividend, divisor);
   }
 
-  verifyAnswer(): boolean {
+  verifyAnswer(): void {
     // @ts-ignore
-    return this.divisionPair.first / this.divisionPair.second === this.result;
+    this.answer.registered = true;
+    this.answer.correct = this.divisionPair.first / this.divisionPair.second === this.answer.input;
   }
 
 }
