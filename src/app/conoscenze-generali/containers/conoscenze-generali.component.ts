c58e726db3ec92d3a56d25419af5aff2be290618
Uygar Karalalek <uy.kara@gmail.com>
2021-05-26 10:59:24 +0200

    Added foto conoscenze-generali

--- src/app/conoscenze-generali/containers/conoscenze-generali.component.ts
+++ src/app/conoscenze-generali/containers/conoscenze-generali.component.ts
@@ -9,9 +9,9 @@ export class ConoscenzeGeneraliComponent implements OnInit {
 
   cards = [
     {path: '/conoscenze-generali/leggere-orologio', image: 'assets/conoscenzeGenerali/leggere-orologio.png', title: 'Leggere l\'orologio'},
-    {path: '/conoscenze-generali/misure-lunghezza', image: 'assets/conoscenzeGenerali/conoscenzeGenerali.jpeg', title: 'Misure di lunghezza'},
-    {path: '/conoscenze-generali/misure-peso', image: 'assets/conoscenzeGenerali/conoscenzeGenerali.jpeg', title: 'Misure di peso'},
-    {path: '/conoscenze-generali/riconoscere-soldi', image: 'assets/conoscenzeGenerali/conoscenzeGenerali.jpeg', title: 'Riconoscere i soldi'},
+    {path: '/conoscenze-generali/misure-lunghezza', image: 'assets/conoscenzeGenerali/righello.png', title: 'Misure di lunghezza'},
+    {path: '/conoscenze-generali/misure-peso', image: 'assets/conoscenzeGenerali/bilancia.png', title: 'Misure di peso'},
+    {path: '/conoscenze-generali/riconoscere-soldi', image: 'assets/conoscenzeGenerali/money.png', title: 'Riconoscere i soldi'},
   ];
 
   constructor() {
